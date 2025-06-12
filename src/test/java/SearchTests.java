670344ac967f25290f10ce9bb1c158672e37043a
Constantin Bessonov <beskos@mail.ru>
2025-06-12 11:10:31 +0300

    init commit

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+    @Test
+    void successfulAntibotTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("html").shouldHave(text("Об этой странице"));
+    }
+
+    @Test
+    void name() {
+
+    }
+}
